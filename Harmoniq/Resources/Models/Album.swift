32eb069f02157d42b83c6030d63168328642a14b
lashu333 <140723330+lashu555@users.noreply.github.com>
2025-02-24 15:36:37 +0400

    added album model
    fetch-ready

---
+++ Harmoniq/Resources/Models/Album.swift
@@ -0,0 +1,35 @@
+//
+//  Album.swift
+//  Harmoniq
+//
+//  Created by Lasha Tavberidze on 24.02.25.
+//
+
+import Foundation
+
+// MARK: - AlbumElement
+struct AlbumElement: Codable {
+    let releaseYear, name, artist: String
+    let songs: [Song]
+    let image: String
+    let id: String
+
+    enum CodingKeys: String, CodingKey {
+        case releaseYear = "release_year"
+        case name, artist, songs, image, id
+    }
+}
+
+// MARK: - Song
+struct Song: Codable {
+    let title: String
+    let url: String
+    let duration, id, albumID: String
+
+    enum CodingKeys: String, CodingKey {
+        case title, url, duration, id
+        case albumID = "albumId"
+    }
+}
+
+typealias Album = [AlbumElement]
